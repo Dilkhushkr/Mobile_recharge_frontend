4a830cd555caa8b83e6fafa22b07840d652c1930
Dilkhushkr <128610575+Dilkhushkr@users.noreply.github.com>
2026-04-04 02:55:54 +0530

    jenkin ci_cd pipline

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -109,7 +109,7 @@ const Dashboard: React.FC = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        🚗 New car  Rental Dashboard
+        🚗 All type of car   Rental Dashboard
       </motion.h1>
       
       
